81f6dabd29e02586ba7c5e34c62b98aa86b9f7ba
Saxogoal <andrepiros01@gmail.com>
2026-05-21 15:27:30 -0300

    finished the learning activity

---
+++ W03/scripts/modules.mjs
@@ -0,0 +1,20 @@
+import byuiCourse from "./course.mjs"; // Default import for default export
+import { setSectionSelection } from "./sections.mjs"; // Named import
+import { setTitle, renderSections } from "./output.mjs"; // Named imports
+
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    byuiCourse.changeEnrollment(sectionNum);
+    renderSections(byuiCourse.sections);
+});
+
+document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = Number(document.querySelector("#sectionNumber").value);
+    byuiCourse.changeEnrollment(sectionNum, false);
+    renderSections(byuiCourse.sections);
+});
+
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
