ce4664866941be3c4b1ca65c83cd5a7402667794
Saxogoal <andrepiros01@gmail.com>
2026-05-22 18:22:59 -0300

    finished

--- W03/scripts/weather.js
+++ W03/scripts/weather.js
@@ -1,24 +1,26 @@
+// HTML elements to manipulate
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75657335563&lon=6.653147199720288&units=imperial&appid=87752bf08c51779202e8da8f3ad6e63f';
 
+// Required variables for API call
+const apiKey = "87752bf08c51779202e8da8f3ad6e63f"
+const lat = 49.75657335563;
+const lon = 6.653147199720288;
+
+
+//Constructing the URL for the API call
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+// Fetching the weather data from the API
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); 
-            function displayResults(data) {
-                currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-
-                const iconCode = data.weather[0].icon;
-                weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${iconCode}@2x.png`);
-                weatherIcon.setAttribute('alt', data.weather[0].description);
-
-                captionDesc.textContent = data.weather[0].description;
-            }
+            console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -27,7 +29,16 @@ async function apiFetch() {
     }
 }
 
-apiFetch();
+// Displaying the results on the webpage
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+
+    captionDesc.textContent = data.weather[0].description;
+}
 
 apiFetch();
 
